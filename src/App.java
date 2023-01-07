cb3eeb1a978b3483c26a557d35855b9c50e7a193
Moeen <moeenyounis11@gmail.com>
2023-01-07 23:42:52 +0000

    `App`: updated following b139e04

--- src/App.java
+++ src/App.java
@@ -8,15 +8,13 @@ public class App {
     public static void main(String[] args) {
         // A test `Message` object is created and displayed
         final Message message = new Message(
-            "5103d1029e1c05d5e0a72833fbe6ed727266f1f333a64c3af2243ae6c1918b65",
             "@m1younis",
             null,
             "#testing",
             null,
-            1672766344,
             "This, is, a, message, for, testing!".split(", ")
         );
 
-        System.out.println(message.formattedBody('r'));
+        System.out.print(message.getBody());
     }
 }
