429b1e15c62eddd6e52bef80466dac3a2967cb49
Moeen <moeenyounis11@gmail.com>
2023-01-10 08:00:10 +0000

    `controllers.MessageController`: `storeMessage` implemented

--- src/controllers/MessageController.java
+++ src/controllers/MessageController.java
@@ -1,6 +1,22 @@
 
 package controllers;
 
+import models.Message;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MessageController {
     public static final String LOCAL_MESSAGES = "messages.txt";
+
+    public static void storeMessage(Message message, boolean append) {
+        try {
+            // The `append` parameter is used to distinguish between writing to the local storage
+            // file depending on whether it already exists
+            final FileWriter writer = new FileWriter(LOCAL_MESSAGES, append);
+            writer.write(message.getBody() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
