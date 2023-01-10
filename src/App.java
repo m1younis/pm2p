509f288643ba6dac62c96560d6ce3d2cf1e8c862
Moeen <moeenyounis11@gmail.com>
2023-01-10 08:05:05 +0000

    `App`: updated following 429b1e1

--- src/App.java
+++ src/App.java
@@ -1,19 +1,38 @@
 
+import controllers.MessageController;
 import models.Message;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * The project's entry point, responsible for launching the application.
  */
 public class App {
     public static void main(String[] args) {
-        // A test `Message` object is created and displayed
+        // The testing `Message` object is initialised to be stored (given the local storage file
+        // doesn't already exist) and displayed
         final Message message = new Message(
+            "5103d1029e1c05d5e0a72833fbe6ed727266f1f333a64c3af2243ae6c1918b65",
             "@m1younis",
             null,
             "#testing",
             null,
+            1672766344,
             "This, is, a, message, for, testing!".split(", ")
         );
+
+        try {
+            if (new File(MessageController.LOCAL_MESSAGES).createNewFile()) {
+                MessageController.storeMessage(message, false);
+                System.out.printf(
+                    "`%s` created with Message-uid: SHA-256 %s\n",
+                    MessageController.LOCAL_MESSAGES,
+                    message.getHash()
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.print(message.getBody());
     }
